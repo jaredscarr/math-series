8ce2872f3b1ff58ecc3e7e807d659c36a9e3b1f9
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-01 15:37:35 -0800

    added test for sum_series that passes now with sum_series method.

--- series.py
+++ series.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
 
+# def do_the_math(n, num1, num2):
+#     """Return nth number in sequence of two given numbers."""
+#     num1 = num1
+#     num2 = num2
+#     for numbers in range(0, n):
+#         total = num1
+#         num1 = num2
+#         num2 += total
+#     return num1
+
+
 def fibonacci(n):
     """Return nth number in the Fibonaci sequence."""
     num1 = 0
@@ -16,6 +27,17 @@ def lucas(n):
     """Return nth number in the Lucas sequence."""
     num1 = 2
     num2 = 1
+    for numbers in range(0, n):
+        total = num1
+        num1 = num2
+        num2 += total
+    return num1
+
+
+def sum_series(n, num1=0, num2=1):
+    """Return nth number in sequence of two given numbers."""
+    num1 = num1
+    num2 = num2
     for numbers in range(0, n):
         total = num1
         num1 = num2
--- test_series.py
+++ test_series.py
@@ -14,6 +14,12 @@ LUCAS = [
     (2, 3),
 ]
 
+OPTIONS = [
+    (0, 47, 23, 47),
+    (0, 1, 10, 1),
+    (0, 4, 78, 4),
+]
+
 
 @pytest.mark.parametrize('n, result', FIB_NUMS)
 def test_fibonacci(n, result):
@@ -27,3 +33,10 @@ def test_lucas_nums(n, result):
     """Test lucas method."""
     from series import lucas
     assert lucas(n) == result
+
+
+@pytest.mark.parametrize('n, num1, num2, result', OPTIONS)
+def test_sum_series(n, num1, num2, result):
+    """Test sum_series method to see which series is selected."""
+    from series import sum_series
+    assert sum_series(n, num1, num2) == result
