1f725b0b1d5d6b0c348557511062ca82c1d56095
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-03-01 16:35:38 -0800

    the __name__ attribute block added and the methods in the code have been explained"

--- series.py
+++ series.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-# def do_the_math(n, num1, num2):
-#     """Return nth number in sequence of two given numbers."""
-#     num1 = num1
-#     num2 = num2
-#     for numbers in range(0, n):
-#         total = num1
-#         num1 = num2
-#         num2 += total
-#     return num1
-
-
 def fibonacci(n):
     """Return nth number in the Fibonaci sequence."""
     num1 = 0
@@ -43,3 +32,14 @@ def sum_series(n, num1=0, num2=1):
         num1 = num2
         num2 += total
     return num1
+
+
+if __name__ == "__main__":
+    """This module defines functions that implement mathematical series."""
+    print("This module defines functions that implement mathematical series.")
+    print("The fibonacci method returns the nth number in the sequence")
+    print("fibonacci(8) is", fibonacci(8))
+    print("The lucus method returns the nth number in the sequence")
+    print("lucas(10) is", lucas(10))
+    print("The sum_series method returns the nth number of two given numbers.")
+    print("sum_series(39, 23, 67) ", sum_series(39, 23, 67))
